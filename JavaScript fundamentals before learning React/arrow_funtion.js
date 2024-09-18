9d054cbb6a9c4e8159533295c256263da289ea8a
alberthgrande <alberthgrande98@gmail.com>
2024-09-18 13:19:18 +0800

    [Added] arrow_funtion.js

---
+++ JavaScript fundamentals before learning React/arrow_funtion.js
@@ -0,0 +1,16 @@
+// JavaScript ES5 function
+function getGreeting1() {
+  return `Welcome to JavaScript`;
+}
+
+// JavaScript ES5 function
+const getGreeting2 = () => {
+  return `Welcome to JavaScript`;
+};
+
+// JavaScript ES6 arrow function without body and implicit return
+const getGreeting3 = () => `Welcome to JavaScript`;
+
+console.log(getGreeting1());
+console.log(getGreeting2());
+console.log(getGreeting3());
