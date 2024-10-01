d1a22a84a8e8ca65de3e280a72e9b70d162182e5
alberthgrande <alberthgrande98@gmail.com>
2024-10-01 21:40:08 +0800

    [Added] src/components/HelloWorld.jsx

---
+++ hacker-stories/src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const title = "React";
+
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello {title}</h1>
+    </div>
+  );
+}
+export default HelloWorld;
