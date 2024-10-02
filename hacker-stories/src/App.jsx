2367986bc086261a5bc79d467c2652a3cdba9895
alberthgrande <alberthgrande98@gmail.com>
2024-10-02 14:46:55 +0800

    [Modified] src/App.jsx added ReactList.jsx

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,13 +2,18 @@ import * as React from "react";
 import BeginnerVariables from "./components/BeginnerVariables";
 import AdvancedVariables from "./components/AdvancedVariables";
 import HelloWorld from "./components/HelloWorld";
+import ReactList from "./components/ReactList";
 
 function App() {
   return (
     <>
       <BeginnerVariables />
+      <hr />
       <AdvancedVariables />
+      <hr />
       <HelloWorld />
+      <hr />
+      <ReactList />
     </>
   );
 }
