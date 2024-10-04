ac9d70ca68bf172882236e21fa5626abbdb32ffa
alberthgrande <alberthgrande98@gmail.com>
2024-10-04 19:01:27 +0800

    [Modified] src/App.jsx create search props component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -3,10 +3,13 @@ import BeginnerVariables from "./components/BeginnerVariables";
 import AdvancedVariables from "./components/AdvancedVariables";
 import HelloWorld from "./components/HelloWorld";
 import ReactList from "./components/ReactList";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 import ReactProps from "./components/ReactProps";
 
 function App() {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <>
       <BeginnerVariables />
@@ -17,11 +20,32 @@ function App() {
       <hr />
       <ReactList />
       <hr />
-      <Search />
+      <Search onSearch={handleChange} />
       <hr />
       <ReactProps />
     </>
   );
 }
+
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    props.onSearch(event);
+    console.log(event.target.value);
+  };
+
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+      <br />
+      <p>
+        Search for <strong>{searchTerm}</strong>.
+      </p>
+    </>
+  );
+};
 
 export default App;
