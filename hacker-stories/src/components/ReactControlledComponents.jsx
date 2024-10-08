ba611838c2166c54dd89f3504268b58f10edab31
alberthgrande <alberthgrande98@gmail.com>
2024-10-08 19:18:12 +0800

    [Added] src/components/ReactControlledComponents.jsx

---
+++ hacker-stories/src/components/ReactControlledComponents.jsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+const ReactControlledComponents = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  //     state     , state updater function,   initial state
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredStories = stories.filter(
+    (story) =>
+      story.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      story.author.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search search={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={filteredStories} />
+    </div>
+  );
+};
+
+const Search = (props) => {
+  return (
+    <div>
+      <label htmlFor="search">Search</label>
+      <input
+        type="text"
+        id="search"
+        value={props.search}
+        onChange={props.onSearch}
+      />
+    </div>
+  );
+};
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+const Item = (props) => {
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
+
+export default ReactControlledComponents;
